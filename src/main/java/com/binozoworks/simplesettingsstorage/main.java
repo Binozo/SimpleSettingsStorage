16813db175c458697b30d4760562e03cc5c7b2c9
Binozo <noah.czaplewski05@gmail.com>
2021-09-18 15:25:51 +0200

    main.java test program

--- src/main/java/com/binozoworks/simplesettingsstorage/main.java
+++ src/main/java/com/binozoworks/simplesettingsstorage/main.java
@@ -1,7 +1,16 @@
 package com.binozoworks.simplesettingsstorage;
 
+import java.io.IOException;
+
 public class main {
     public static void main(String[] args) {
-
+        //for testing
+        try {
+            SimpleSettingsStorage simplesettingsstorage = new SimpleSettingsStorage();
+            simplesettingsstorage.getStorage().put("test", "value");
+            simplesettingsstorage.saveStorage(); //never forget that!
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
